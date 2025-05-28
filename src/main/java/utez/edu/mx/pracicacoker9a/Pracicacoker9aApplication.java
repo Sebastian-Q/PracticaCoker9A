ddf526cb83907e17c19d781e8241d5b59d147285
Sebastian-Q <20203tn049@utez.edu.nx>
2025-05-28 17:47:11 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/pracicacoker9a/Pracicacoker9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.pracicacoker9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pracicacoker9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pracicacoker9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/pracicacoker9a/Pracicacoker9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.pracicacoker9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pracicacoker9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
